a894f42136dae79daff3f4cf4ad4a2e78fe9b4ea
Igor Klymchuk <i2n6g0v1a9r5@gmail.com>
2023-02-12 16:10:03 +0200

    my first commit

---
+++ src/com/epam/learn/main/InputProgram.java
@@ -0,0 +1,17 @@
+package com.epam.learn.main;
+
+import java.io.IOException;
+
+public class InputProgram {
+    public static void main(String[] args) {
+        int x;
+        try {
+            x = System.in.read();
+            System.out.println("code ="+ x + " x=" + (char)x);
+        } catch (IOException e) {
+          //  throw new RuntimeException(e);
+            e.printStackTrace();
+        }
+
+    }
+}
